f0836e94a2672409a911238e6446bf6680178dc9
Nick <nsagias@gmail.com>
2022-01-25 23:50:25 -0500

    create mousemove ftsx and add to app, remove title from app

--- hooks-helpers/src/App.tsx
+++ hooks-helpers/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import MouseMove from './components/MouseMove';
 import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
-      <Title />
+      <MouseMove />
     </div>
   );
 }
---
+++ hooks-helpers/src/components/MouseMove.tsx
@@ -0,0 +1,9 @@
+const MouseMove = () => {
+  return (
+    <div>
+      <h2>useMousePosition</h2>
+    </div>
+  )
+};
+
+export default MouseMove;
