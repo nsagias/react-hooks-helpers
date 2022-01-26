a728cd56c5c87933ad9ca1f9aeee1d910e7238bf
Nick <nsagias@gmail.com>
2022-01-26 12:45:38 -0500

    import use effect, add addEventListener, create empty mouseMoveHander

--- hooks-helpers/src/components/MouseMove.tsx
+++ hooks-helpers/src/components/MouseMove.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+
 const MouseMove = () => {
+
+  useEffect(() => {
+    const mouseMoveHander = () => {};
+    document.addEventListener('mousemove', mouseMoveHander);
+  }, []);
+
   return (
     <div>
       <h2>useMousePosition</h2>
+      <h2>x: 0, y: 0</h2>
     </div>
   )
 };
