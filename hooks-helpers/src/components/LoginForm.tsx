68801914a5995aa676045ab618d37599d75e7478
Nick <nsagias@gmail.com>
2022-01-26 20:40:33 -0500

    inport useInput and spread object

--- hooks-helpers/src/components/LoginForm.tsx
+++ hooks-helpers/src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
-import { setPriority } from "os";
-import { useState } from "react";
+// import { useState } from "react";
+import useInput from "../hooks/useInput";
 
 interface IHandleSubmit {
   (event:any): void;
@@ -9,14 +9,16 @@ const LoginForm = () => {
   const USERNAME_DEFAULT: string = "";
   const PASSWORD_DEFAULT: string = "";
 
-  const [userName, setUserName] = useState<string>(USERNAME_DEFAULT);
-  const [password, SetPassword] = useState<string>(PASSWORD_DEFAULT);
+  // const [userName, setUserName] = useState<string>(USERNAME_DEFAULT);
+  // const [password, SetPassword] = useState<string>(PASSWORD_DEFAULT);
+  const userName = useInput(USERNAME_DEFAULT);
+  const password = useInput(PASSWORD_DEFAULT);
 
 
   const handleSubmit: IHandleSubmit = (event:any) => {
-    setUserName(USERNAME_DEFAULT);
-    SetPassword(PASSWORD_DEFAULT);
-    alert(`Submiting ${userName} and ${password}`);
+    // setUserName(USERNAME_DEFAULT);
+    // SetPassword(PASSWORD_DEFAULT);
+    alert(`Submiting ${userName.value} and ${password.value}`);
     event.preventDefault();
   };
 
@@ -26,15 +28,19 @@ const LoginForm = () => {
       <form onSubmit={handleSubmit}>
         <label>User Name:</label>
         <input 
-          value={userName}
-          onChange={(event) => setUserName(event.target.value)}
+          // value={userName.value}
+          // onChange={(event) => setUserName(event.target.value)}
+          // onChange={userName.onChange}
+          { ...userName }
         />
         <br />
         <label>Password:</label>
         <input 
-          value={password}
-          onChange={(event) => SetPassword(event.target.value)}
-        />
+          // value={password.value}
+          // onChange={(event) => SetPassword(event.target.value)}
+          // onChange={password.onChange}
+          { ...password}
+          />
         <br />
         <button type="submit">Login</button>
       </form>
