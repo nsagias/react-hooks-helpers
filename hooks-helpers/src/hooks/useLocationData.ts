1ba15e284741777a0e556aaa5898fcbce32df0f9
Nick <nsagias@gmail.com>
2022-01-26 23:26:14 -0500

    scaffold hook

---
+++ hooks-helpers/src/hooks/useLocationData.ts
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+const useLocationData = () => {
+  const [coordinates, setCoordinates] = useState();
+  useEffect(() => {});
+
+}
+export default useLocationData;
