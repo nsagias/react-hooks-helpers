5730a2770f31b5c8d9737179c1a86d1600116668
Nick <nsagias@gmail.com>
2022-01-26 20:43:50 -0500

    create clear value function and add typescript

--- hooks-helpers/src/hooks/useInput.ts
+++ hooks-helpers/src/hooks/useInput.ts
@@ -4,17 +4,26 @@ interface IOnChange {
   (event:any): void;
 }
 
+interface IClearSetValue {
+  (valueDefault: any): void;
+}
+
 
 const useInput = (initialvalue: any) => {
   const [value, setValue] = useState(initialvalue);
 
   const onChange: IOnChange = (event:any) => {
     setValue(event.target.value);
-  }
+  };
+
+  const clearSetValue: IClearSetValue = (valueDefault: any) => {
+    setValue(valueDefault)
+  };
 
   return { 
     value, 
-    onChange 
+    onChange,
+    clearSetValue 
   };
 };
 
