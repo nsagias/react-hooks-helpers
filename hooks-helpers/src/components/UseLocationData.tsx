918c425615965d3a96bfe6e9f0557c4d198a63ae
Nick <nsagias@gmail.com>
2022-01-26 23:41:40 -0500

    import useLocationData and display latitude and longitude

--- hooks-helpers/src/components/UseLocationData.tsx
+++ hooks-helpers/src/components/UseLocationData.tsx
@@ -1,10 +1,11 @@
 import useLocationData from "../hooks/useLocationData";
 
 const LocationData = () => {
-  useLocationData
+  const {lat, lon} = useLocationData();
   return (
     <div>
       <h1>useLocationData</h1>
+      <h2>Latitude: {lat}  Longtitude: {lon} </h2>
     </div>
   );
 };
