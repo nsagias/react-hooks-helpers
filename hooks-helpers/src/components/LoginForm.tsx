046e410ee012c426004c8bd85f53dbff1b6c4d96
Nick <nsagias@gmail.com>
2022-01-26 20:05:06 -0500

    Add alert to handlesubmit for testing, add label and input for username and password

--- hooks-helpers/src/components/LoginForm.tsx
+++ hooks-helpers/src/components/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { setPriority } from "os";
 import { useState } from "react";
 
 interface IHandleSubmit {
@@ -5,7 +6,7 @@ interface IHandleSubmit {
 }
 
 const LoginForm = () => {
-  const USERNAME_DEFAULT: string = "" ;
+  const USERNAME_DEFAULT: string = "";
   const PASSWORD_DEFAULT: string = "";
 
   const [userName, setUserName] = useState<string>(USERNAME_DEFAULT);
@@ -13,14 +14,27 @@ const LoginForm = () => {
 
 
   const handleSubmit: IHandleSubmit = (event:any) => {
-    event.preventDefault()
+    event.preventDefault();
+    alert(`Submiting ${userName} and ${password}`);
   };
 
   return (
     <div>
       <h2>Login Form</h2>
       <form onSubmit={handleSubmit}>
-
+        <label>User Name:</label>
+        <input 
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <br />
+        <label>Password:</label>
+        <input 
+          value={password}
+          onChange={(event) => SetPassword(event.target.value)}
+        />
+        <br />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
