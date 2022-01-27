fb7e93fb31354bc20e294bb91885ac214319f34b
Nick <nsagias@gmail.com>
2022-01-26 19:49:07 -0500

    Create form skeleton and handleSubmit preventDefault

--- hooks-helpers/src/components/LoginForm.tsx
+++ hooks-helpers/src/components/LoginForm.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
 const LoginForm = () => {
+
+  const handleSubmit = (event:any) => {
+    event.preventDefault()
+  }
+
   return (
     <div>
       <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+
+      </form>
     </div>
   );
 };
