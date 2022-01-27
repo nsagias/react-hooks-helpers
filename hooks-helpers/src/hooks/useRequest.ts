13eb3af49448ee75b72781e6b74ea451ae1d9258
Nick <nsagias@gmail.com>
2022-01-26 23:47:28 -0500

    Scaffold hook with generic x

---
+++ hooks-helpers/src/hooks/useRequest.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState} from "react";
+
+const useRequest = () => {
+  const X = ""; 
+  const [x, setX] = useState();
+  useEffect( () => {}, []); 
+
+}
+
+export default useRequest;
