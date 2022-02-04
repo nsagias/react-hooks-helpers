3cd5f38121ed4ca0c58454743b7084a1e444a1b4
Nick <nsagias@gmail.com>
2022-02-04 00:58:07 -0500

    update mouseMoveHandler with MouseEvent type

--- hooks-helpers/src/hooks/useMouseMove.ts
+++ hooks-helpers/src/hooks/useMouseMove.ts
@@ -4,12 +4,13 @@ interface IMousePosition {
   x: number;
   y: number;
 }
+
 const useMouseMove = () => {
   const MOUSE_START_DEFAULT: IMousePosition = {x: 0, y: 0};
   const [mousePosition, setMousePosition] = useState<IMousePosition>(MOUSE_START_DEFAULT);
   
   useEffect(() => {
-    const mouseMoveHandler = (event:any) => {
+    const mouseMoveHandler = (event: MouseEvent) => {
       setMousePosition({
         x: event.clientX,
         y: event.clientY
